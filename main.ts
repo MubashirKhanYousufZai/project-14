93cd01b03d099c5b215cb98f0f8684a4c0531f6b
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-06-06 03:34:44 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,14 @@
+            //   LOGICAL OPERTOR:
+            let AliBudget = 300000;
+            let HaveBike = true;
+            console.log(AliBudget >= 200000 && HaveBike == true);
+            console.log(AliBudget <= 400000 && HaveBike == true);
+            
+
+
+            let ArmanBudget =100000;
+            let HaveGirl = false;
+            console.log(ArmanBudget >= 1100000 && HaveGirl == false);
+            console.log(ArmanBudget <= 900000 && HaveGirl == false);
+            
+            
